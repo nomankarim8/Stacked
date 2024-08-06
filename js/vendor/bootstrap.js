3a314e7d46ee78adae663eacd0f17b6cbc086175
nomankarim8 <karimnoman97@gmail.com>
2024-08-05 19:35:39 -0700

    update bootstrap.js

--- js/vendor/bootstrap.js
+++ js/vendor/bootstrap.js
@@ -1508,7 +1508,7 @@ if (typeof jQuery === 'undefined') {
       var rightEdgeOffset = pos.left + viewportPadding + actualWidth
       if (leftEdgeOffset < viewportDimensions.left) { // left overflow
         delta.left = viewportDimensions.left - leftEdgeOffset
-      } else if (rightEdgeOffset > viewportDimensions.width) { // right overflow
+      } else if (rightEdgeOffset > viewportDimensions.width) { 
         delta.left = viewportDimensions.left + viewportDimensions.width - rightEdgeOffset
       }
     }
