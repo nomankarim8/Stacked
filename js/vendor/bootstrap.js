a9fac424bccf04e69a6209ec322d68a18a76aa0c
nomankarim8 <karimnoman97@gmail.com>
2024-12-20 21:12:52 -0800

    update bootstrap.js

--- js/vendor/bootstrap.js
+++ js/vendor/bootstrap.js
@@ -3,7 +3,6 @@ if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
 
-
 +function ($) {
   var version = $.fn.jquery.split(' ')[0].split('.')
   if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1)) {
